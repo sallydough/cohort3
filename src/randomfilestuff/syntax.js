dbad722704bb366c9a651efa53b396a3fcd03510
Sally Do <sallydo@Sallys-MacBook-Pro.local>
2019-10-16 13:44:31 -0600

    add new randomstuff

---
+++ src/randomfilestuff/syntax.js
@@ -0,0 +1,83 @@
+const functions = {
+
+variableNumb: (num1,num2) => {
+    return num1 + num2;
+},
+
+stringTest: (name) => {
+    return name;
+},
+
+booleanTest: (number) => {
+    if (number >= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+arrayTest: () => {
+    let arraynumb = 0
+    [item1, item2, item3, item4, item5];
+    return arrayTest[] 
+}
+
+dictionaryObjectTest: () => {
+
+
+
+
+}
+
+
+} ;
+export default functions;
+
+
+
+// be creative; write tests and code to prove to yourself that you understand the concepts of each line. Remember the steps:
+// write a stub
+// write a test that fails
+// write the code for the test to pass
+// run the test
+// repeat
+// (we will soon start learning about a concept called refactoring)
+// make sure your code and comments are very very very aesthetically pleasing. 
+// We will be comparing (but it’s not a competition). 
+// Comments should not have the details we can read in the code but should explain the approach or the concept. 
+// Work in pairs / groups to ensure you are incorporating everyone's great ideas. 
+// Be creative. Write some awesome code and tests and show off your results. 
+// We will be doing group code reviews.
+// when debugging your code insert a good text description to describe where you are in the code and what parameter you are spitting out in your console logs. 
+
+
+
+
+
+
+
+
+// define attributes / variables
+// number
+// string
+// boolean
+// array
+// dictionary / objects
+// undefined
+// sample if / else
+// functions
+// parameters
+// returns
+// arrays
+// add to the front
+// add to the end
+// update values
+// loops 
+// for
+// for/in
+// while
+// do while
+// forEach (with array and function)
+// Objects / Dictionaries
+// declare object
+// lookup key to retrieve value
