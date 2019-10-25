248f2da4a3afe3ac36c2e69dc862f8a3dca5b156
Sally Do <sallydo@Sallys-MacBook-Pro.local>
2019-10-25 16:06:21 -0600

    main/test/js/html 1025tdd

---
+++ src/javascript/10_25_tddmain.js
@@ -0,0 +1,18 @@
+import functions from './10_25_tdd.js'
+
+
+console.log("Hello World from 10_25_tddmain.js");
+console.log("hello World");
+
+//add event listener when I click on 'idDoIt' button: */
+idDoIt.addEventListener("click", () => {
+    //
+    let firstNumInput = Number(idEnterFirstNum.value);
+    let secondNumInput = Number(idEnterSecondNum.value);
+    console.log(firstNumInput, secondNumInput);
+
+});
+// const firstNumberInput = document.getElementById("idEnterFirstNumber");
+// console.log(firstNumberInput);
+// .addEventListener("click", onButtonClicked);
+// console.log("hello world");
