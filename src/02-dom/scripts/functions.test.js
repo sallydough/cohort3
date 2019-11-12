02efeba9d688131b5b2403cc80e0258a79bd0bdf
Sally Do <sallydoh@gmail.com>
2019-11-12 13:07:30 -0700

    DOM Manipulation CSS

--- src/02-dom/scripts/functions.test.js
+++ src/02-dom/scripts/functions.test.js
@@ -4,3 +4,13 @@ test("helloDOMWorld", () => {
     console.log("hey DOM world from test.js");
     functions.helloDOMWorld();
 });
+
+// test("showButtonFunction", () => {
+//     console.log("hey DOM world from test.js");
+//     expect(functions.showButtonFunction()).toBe([li]);
+// });
+
+test("helloDOMWorld", () => {
+    console.log("hey DOM world from test.js");
+    functions.helloDOMWorld();
+});
--- src/02-dom/scripts/main.js
+++ src/02-dom/scripts/main.js
@@ -9,9 +9,30 @@ idBasicDOM.addEventListener("click", () => {
     console.log(event.target);
 });
 
+// let count= 1;
+// idButtonAdd.addEventListener("click", () => {
+//     let newAddedList = document.createElement("li");
+//     idOL.appendChild(newAddedList);
+//     newAddedList.textContent = `Item Number ${count}`;
+//     newAddedList.setAttribute("count", count++)
+// });
+
 let count= 1;
 idButtonAdd.addEventListener("click", () => {
-    let newAddedList = document.createElement("li");
+    let newAddedList = document.createElement("div");
     idOL.appendChild(newAddedList);
-    newAddedList.textContent = `Item Number ${count++}`;
+    newAddedList.textContent = `Item Number ${count}`;
+    newAddedList.setAttribute("count", count++)
+    newAddedList.setAttribute("class", "cardArea")
 });
+
+// let bigDivGetEle = document.getElementById("idBigDiv")
+// let count = 4;
+
+// idAddDiv.addEventListener("click", () => {
+//     let newAddedDiv = document.createElement("div");
+//     // let newCountChild=bigDivGetEle.childElementCount;
+//     bigDivGetEle.appendChild(newAddedDiv);
+//     newAddedDiv.className = "bodyDiv1";
+//     newAddedDiv.textContent = `Card Number ${count}`;
+//     newAddedDiv.setAttribute("count", count++);
