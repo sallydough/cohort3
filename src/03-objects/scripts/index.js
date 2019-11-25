880935b118d47547b6178af2b30cf4cff20c386e
Sally Do <sallydoh@gmail.com>
2019-11-25 15:20:31 -0700

    130B wireframe

--- src/03-objects/scripts/index.js
+++ src/03-objects/scripts/index.js
@@ -1,1 +1,13 @@
 import { Account, functions } from "./acounts.js"
+
+const idAddAcount = document.getElementById("idAddAcount");
+
+idAddAcount.addEventListener("click", () => {
+    // -create a card account:
+    //     -displays name, 
+    //     -total balance, 
+    //     -delete account button
+});
+
+
+
