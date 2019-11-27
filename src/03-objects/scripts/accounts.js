7b14316c53e04ed7515b32208146e50d1ed20e92
Sally Do <sallydoh@gmail.com>
2019-11-27 10:57:35 -0700

    130C functions

--- src/03-objects/scripts/accounts.js
+++ src/03-objects/scripts/accounts.js
@@ -12,16 +12,46 @@ class Account {
         this.accountName = accountName;
         this.balance = startingBalance;
     }
-    deposit(amount) { 
+    deposit(amount) {
         this.balance += amount;
     }
-    withdraw(amount) { 
-        this.balance -= amount; 
+    withdraw(amount) {
+        this.balance -= amount;
     }
-    checkBalance() { 
-        return this.balance; 
+    checkBalance() {
+        return this.balance;
     }
     // method2() { ... }
 }
 
-export { Account, functions };
+class AccountController {
+    constructor() {
+        this.allTotalAccounts = [];
+    }
+    addNewAcccount(accountName, startingBalance) {
+        const newSubAccount = new Account(accountName, startingBalance)
+        this.allTotalAccounts.push(newSubAccount);
+    }
+    removeAccount(selectedAccountInArray) {
+        this.allTotalAccounts.splice(selectedAccountInArray, 1);
+     }
+    sumTotalBalance() {
+        let sumTotalBal = 0;
+        for (const eachAccount of this.allTotalAccounts) {
+            sumTotalBal += eachAccount.balance;
+        }
+        return sumTotalBal;
+     }
+    highestAccount() { 
+        const balanceArr = this.allTotalAccounts.map(each => each.balance);
+        const highestAcc = Math.max(...balanceArr);
+        return highestAcc;
+    }
+    lowestAccount() {const balanceArr = this.allTotalAccounts.map(each => each.balance);
+        const lowestAcc = Math.min(...balanceArr);
+        return lowestAcc; 
+    }
+
+}
+
+export { Account, AccountController, functions };
