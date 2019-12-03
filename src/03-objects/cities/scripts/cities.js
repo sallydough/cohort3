b03a0a6708ff519b296062e890c452dd714d1c92
Sally Do <sallydoh@gmail.com>
2019-12-02 17:29:07 -0700

    130D test/js complete

--- src/03-objects/cities/scripts/cities.js
+++ src/03-objects/cities/scripts/cities.js
@@ -3,73 +3,100 @@ const functions = {
     helloCities: () => {
         console.log("hi cities");
     }
-
 }
 
 class City {
-    constructor(province, name, latitude, longitude, population) {
-        this.province = province;
+    constructor(key, name, latitude, longitude, population) {
+        this.key = key;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
         this.population = population;
+        this.hemisphere = "";
     }
 
     showInfo() {
-        return `Province:${this.province}, 
-        Name:${this.name}, 
-        Latitude:${this.latitude}, 
-        Longitude: ${this.longitude}, 
-        Population: ${this.population}`
+        return `Key:${this.key} || Name:${this.name} || Latitude:${this.latitude} || Longitude:${this.longitude} || Population:${this.population}`
     }
-    movedIn(n) {
-        this.pop += parseInt(n);
+    movedIn(peopleIn) {
+        this.population += parseInt(peopleIn);
     }
-    movedOut(n) {
-        this.pop -= parseInt(n);
+    movedOut(peopleOut) {
+        this.population -= parseInt(peopleOut);
     }
-    howBig() {
-        if (this.population > 100000) return "Big City";
-        else if (this.population > 20000) return "Large town";
+    howBig(population) {
+        if (this.population > 100000) return "City";
+        else if (this.population > 20000) return "Large Town";
         else if (this.population > 1000) return "Town";
         else if (this.population > 100) return "Village";
         else return "Hamlet";
     }
+    whichSphere() {
+        let latitude = this.latitude;
+        if (latitude > 0) {
+            return this.hemisphere = 'Northern Hemisphere';
+        }
+        else { return this.hemisphere = 'Southern Hemisphere'; };
+    }
 }
 
 class Community {
-    constructor(cityName) { 
+    constructor() {
         this.cities = [];
-        this.message = "";
+        this.byName = {};
+        // this.message = "";
         this.counter = 0;
     }
 
-    whichSphere() {
-        if (this.latitude > 0) {
-            return "Northern Hemisphere";
-        } else {
-            return "Southern Hemisphere";
-        }
+    createCity(lastKey, name, latitude, longitude, population) {
+        this.keyCount = lastKey;
+        const newCity = new City(this.keyCount, name, latitude, longitude, population);
+        this.hemisphere = newCity.whichSphere();
+        this.type = newCity.howBig();
+        this.cities.push(newCity);
+        this.byName[name] = newCity;
+        this.message = `${name} has been added.`;
+        return newCity;
+
+        // this.cities.push(new City(key, name, latitude, longitude, population, this.counter));
+        // this.counter++;
     }
-     
-    getMostNorthern() { }
-    getMostSouthern() { }
-    getAllPopulation() { 
+
+    deleteCity(deletedCity) {
+        // delete this.byName[name];
+        // this.message = `${name} has been deleted from your list.`
+        // this.cities.forEach((key, index) => {
+        //     if (key.name === name) {
+        //         this.cities.splice(index, 1);
+        //     }
+        // });
+        this.cities = this.cities.filter((e) => e.name != deletedCity);
+    }
+
+    getMostNorthern() {
+        this.cities.sort(
+            (city1, city2) => city2.latitude - city1.latitude
+        );
+        return this.cities[0];
+    }
+
+    getMostSouthern() {
+        this.cities.sort(
+            (city1, city2) => city2.latitude - city1.latitude
+        );
+        let lastItem = this.cities.length - 1;
+        return this.cities[lastItem];
+    }
+
+    getAllPopulation() {
         const cityArray = [];
         this.cities.forEach(e => {
             cityArray.push(e.population);
             return cityArray;
         });
-        const totalCityPopulation = cityArray.reduce((acctual, current) => acctual + current);
+        const totalCityPopulation = cityArray.reduce((actual, current) => actual + current);
         return totalCityPopulation;
     }
-    createCity(province, name, latitude, longtitude, population) { 
-        this.cities.push(new City(province, latitude, longtitude, population, this.counter));
-        this.counter++;
-    }
-    deleteCity(deletedCity) {
-        this.cities = this.cities.filter((e) => e.name != deleteCity);
-     }
 }
 
 export { functions, City, Community };
--- src/03-objects/cities/scripts/cities.test.js
+++ src/03-objects/cities/scripts/cities.test.js
@@ -1,8 +1,76 @@
-import {functions , City , Community } from "./cities.js"
-
-
+import { functions, City, Community } from "./cities.js"
 
 test("console C&C", () => {
     console.log("hello community and cities from test.js");
     functions.helloCities();
-})
+});
+
+/////////////////////CLASS CITY TESTING (130D)//////////////////////////
+let key = 1;
+
+test('check show', () => {
+
+    let testCity = new City(key, 'Calgary', 51.0447, -114.0719, 1267344);
+    const showStringInfo = testCity.showInfo();
+    expect(showStringInfo).toBe(`Key:1 || Name:Calgary || Latitude:51.0447 || Longitude:-114.0719 || Population:1267344`);
+});
+
+test('check movedIn and movedOut', () => {
+    let testCity = new City(key, 'Calgary', 51.0447, -114.0719, 1267344);
+    expect(testCity.population).toBe(1267344);
+    testCity.movedIn(1000000);
+    expect(testCity.population).toBe(2267344);
+    testCity.movedOut(1000000);
+    expect(testCity.population).toBe(1267344);
+});
+
+test('check how big a city is', () => {
+    let newCity = new City(key, 'Red Deer', 52.2690, -113.8116, 100418);
+    let largeTown = new City(key++, 'Medicine Hat', 50.0421, -110.7197, 63260);
+    let town = new City(key++, 'Strathmore', 51.0378, -113.4004, 13528);
+    let village = new City(key++, 'Flagstaff County', 52.6531, -111.8823, 639);
+    let hamlet = new City(key++, 'Tilt Cove', 49.88499646, -55.622830842, 6);
+
+    expect(newCity.howBig()).toBe('City');
+    expect(largeTown.howBig()).toBe('Large Town');
+    expect(town.howBig()).toBe('Town');
+    expect(village.howBig()).toBe('Village');
+    expect(hamlet.howBig()).toBe('Hamlet');
+});
+
+test('check whichSphere', () => {
+    let northCity = new City(key++, 'Calgary', 51.0447, -114.0719, 1267344);
+    northCity.whichSphere();
+    expect(northCity.hemisphere).toBe('Northern Hemisphere');
+
+    let southCity = new City(key++, 'Sydney', -33.870453, 151.208755, 4741874);
+    southCity.whichSphere();
+    expect(southCity.hemisphere).toBe('Southern Hemisphere');
+});
+
+
+//////////////////CLASS COMMUNITY TESTING 130D ///////////////////////////////
+test('check getPopulation, ', () => {
+    let cityController = new Community();
+    
+    //'createCity test'
+    cityController.createCity(key++, 'Medicine Hat', 50.0421, -110.7197, 63260);
+    cityController.createCity(key++, 'Yellowknife', 62.453972, -114.371788, 19569);
+    cityController.createCity(key++, 'Red Deer', 52.2690, -113.8116, 100418);
+    let sydneyObj = cityController.createCity(key++, 'Sydney', -33.870453, 151.208755, 4741874);
+
+    // CHECK 'GET TOTAL POPULATION'
+    expect(cityController.getAllPopulation()).toBe(4925121);
+
+    //CHECK 'DELETECITY'
+    let mapCities = cityController.cities.map(city => { return city.name; });
+    expect(mapCities).toEqual(['Medicine Hat', 'Yellowknife', 'Red Deer', 'Sydney']);
+    cityController.deleteCity('Red Deer');
+    mapCities = cityController.cities.map(city => { return city.name; });
+    expect(mapCities).toEqual(['Medicine Hat', 'Yellowknife', 'Sydney']);
+
+    // CHECK 'NORTHERN AND SOUTHERN CITIES'
+    expect(cityController.getMostNorthern().name).toBe('Yellowknife');
+    expect(cityController.getMostSouthern().name).toBe('Sydney');
+
+});
