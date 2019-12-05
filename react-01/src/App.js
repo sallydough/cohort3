649682be38bcea9fa48f14c4680a57f83d7ca4b3
Sally Do <sallydoh@gmail.com>
2019-12-05 12:36:35 -0700

    added React Button

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -2,26 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is ____</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.counter = 21;
+  }
+
+  onPushMe = () => {
+    console.log("You pushed me");
+    console.log(this.counter++);
+  }
+    // function App() {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>I am in control of this application and my name is Sally {this.counter++} </h1>
+            <button onClick={this.onPushMe}>
+            Push Me
+          </button>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+        </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+        </a>
+          </header>
+        </div>
+      );
+    }
+  }
+
+
+  export default App;
