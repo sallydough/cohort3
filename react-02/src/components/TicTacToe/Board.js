e26654869ca4a73d5c5d9328b3f7d4c9220dfa85
Sally Do <sallydoh@gmail.com>
2020-01-04 11:02:22 -0700

    TicTacToe Complete

--- react-02/src/components/TicTacToe/Board.js
+++ react-02/src/components/TicTacToe/Board.js
@@ -2,35 +2,52 @@ import React from 'react';
 import Square from './Square'
 
 class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true,
+    //     };
+    // }
 
-  export default Board;
-  
+
+    renderSquare(i) {
+        return <Square
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)} />;
+    }
+
+    render() {
+        // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        // const winner = calculateWinner(this.state.squares);
+        // let status;
+        // if (winner) {
+        //   status = 'Winner: ' + winner;
+        // } else {
+        //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        // }
+
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Board;
--- react-02/src/components/TicTacToe/Square.js
+++ react-02/src/components/TicTacToe/Square.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-      }
-    render() {
-      return (
-        <button className="square" 
-        onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
+// class Square extends React.Component {
+
+//     render() {
+//       return (
+//         <button className="square" 
+//         onClick={() => this.props.onClick()}>
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
 
   export default Square;
