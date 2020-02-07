b89f52cd22c7362730712e5796b2474c8919d1ce
Sally Do <sallydoh@gmail.com>
2020-02-07 15:58:56 -0700

    Fetch/API Exercise complete

--- src/03-objectsSD/citiesSD/scripts/fetch.js
+++ src/03-objectsSD/citiesSD/scripts/fetch.js
@@ -1,12 +1,24 @@
-const data = {
-    [{"name":"Isabella","surname":"Levy","gender":"female","region":"New Zealand"},
-    {"name":"Major","surname":"Krisztina","gender":"female","region":"Hungary"},
-    {"name":"Ashley","surname":"Gonzales","gender":"female","region":"United States"},
-    {"name":"Ηρακλείδης","surname":"Λιάπης","gender":"male","region":"Greece"},
-    {"name":"Εφιάλτης","surname":"Βονόρτας","gender":"male","region":"Greece"},
-    {"name":"Emilia","surname":"Șerbănescu","gender":"female","region":"Romania"},
-    {"name":"Dezider","surname":"Repiský","gender":"male","region":"Slovakia"},
-    {"name":"Bikram","surname":"Acharya","gender":"male","region":"Nepal"},
-    {"name":"Μέντωρ","surname":"Ακρίδας","gender":"male","region":"Greece"},
-    {"name":"Patricia","surname":"Teleanu","gender":"female","region":"Romania"}]
+const functions = {
+
+    getFirstName: (data) => {
+    let firstName = data[0].name;
+    return firstName;
+    },
+
+    getAllFirstNames: (data) => {
+    return data.map(o => o.name);
+
+
+
+
+
+
+
+    }
 };
+
+export default functions;
+
+const me = 
+{"name":"Kelly","surname":"Sally","gender":"whale","region":"New Zealand"}
+
---
+++ src/03-objectsSD/citiesSD/scripts/fetch.test.js
@@ -0,0 +1,23 @@
+import functions from "./fetch.js"
+
+const data = [
+    {"name":"Isabella","surname":"Levy","gender":"female","region":"New Zealand"},
+    {"name":"Major","surname":"Krisztina","gender":"female","region":"Hungary"},
+    {"name":"Ashley","surname":"Gonzales","gender":"female","region":"United States"},
+    {"name":"Ηρακλείδης","surname":"Λιάπης","gender":"male","region":"Greece"},
+    {"name":"Εφιάλτης","surname":"Βονόρτας","gender":"male","region":"Greece"},
+    {"name":"Emilia","surname":"Șerbănescu","gender":"female","region":"Romania"},
+    {"name":"Dezider","surname":"Repiský","gender":"male","region":"Slovakia"},
+    {"name":"Bikram","surname":"Acharya","gender":"male","region":"Nepal"},
+    {"name":"Μέντωρ","surname":"Ακρίδας","gender":"male","region":"Greece"},
+    {"name":"Patricia","surname":"Teleanu","gender":"female","region":"Romania"}
+];
+
+test ("firstName", () => { 
+    expect(functions.getFirstName(data)).toBe("Isabella");
+})
+
+test ("allFirstName", () => { 
+    //map,sort,filter,reducer(),every() & some();
+    expect(functions.getAllFirstNames(data)).toBe(["Isabella", "Major", "Ashley", "Ηρακλείδης", "Εφιάλτης", "Emilia", "Dezider", "Bikram", "Μέντωρ", "Patricia"]);
+})
